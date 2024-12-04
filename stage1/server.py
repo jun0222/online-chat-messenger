b57fba7b279da5974689c9cf20803f67872f2b91
JunHatanaka <nextseason6@gmail.com>
2024-12-05 00:52:50 +0900

    feat: server側でメッセージ取得

--- stage1/server.py
+++ stage1/server.py
@@ -16,6 +16,18 @@ while True:
    data, address = sock.recvfrom(4096)
    print('メッセージを受信 {} バイト:  {}'.format(len(data), address))
    print('内容: {}'.format(data.decode('utf-8')))
+   
+   # 最初の1バイトを取得し、ユーザー名のバイト数を取得
+   username_len = data[0]
+   print('ユーザー名のバイト数: {}'.format(username_len))
+
+   # username_lenの次のバイトからユーザー名を取得
+   username = data[1:username_len + 1]
+   print('ユーザー名: {}'.format(username.decode('utf-8')))
+
+   # メッセージを取得
+   chat_message = data[username_len + 1:]
+   print('メッセージ: {}'.format(chat_message.decode('utf-8')))
 
    # クライアントへのデータ送信
    if data:
