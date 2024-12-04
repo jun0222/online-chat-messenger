73375d3d80ff2da3b86e55ddc1b9d53a25a6898a
JunHatanaka <nextseason6@gmail.com>
2024-12-04 23:56:46 +0900

    feat: message to Japanese

--- stage1/client.py
+++ stage1/client.py
@@ -10,25 +10,26 @@ server_address = '0.0.0.0'
 server_port = 9001
 
 # 送信するメッセージ
-message = b'Message to send to the client.'
+message = 'クライアントから送信したメッセージです'.encode('utf-8')
 
 # 空の文字列も0.0.0.0として使用できる
 sock.bind((address,port))
 
 try:
-  print('sending {!r}'.format(message))
+  print('送信: {}'.format(message.decode('utf-8')))
 
   # サーバへのデータ送信
   sent = sock.sendto(message, (server_address, server_port))
-  print('Send {} bytes'.format(sent))
+  print('送信 {} バイト'.format(sent))
 
   # 応答を受信
-  print('waiting to receive')
+  print('サーバからの応答を待っています...')
 
   # サーバからのデータ受信
   data, server = sock.recvfrom(4096)
-  print('received {!r}'.format(data))
+  print('受信しました: {}'.format(data.decode('utf-8')))
+
 
 finally:
-  print('closing socket')
+  print('接続終了')
   sock.close()
--- stage1/server.py
+++ stage1/server.py
@@ -4,20 +4,20 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_address = '0.0.0.0'
 server_port = 9001
-print('starting up on port {}'.format(server_port))
+print('サーバー起動。ポート番号: {}'.format(server_port))
 
 # ソケットを特殊なアドレス0.0.0.0とポート9001に紐付け
 sock.bind((server_address, server_port))
 
 while True:
-   print('\nwaiting to receive message')
+   print('メッセージ返信待機中...')
 
    # クライアントからのデータ受信
    data, address = sock.recvfrom(4096)
-   print('received {} bytes from {}'.format(len(data), address))
-   print(data)
+   print('メッセージを受信 {} バイト:  {}'.format(len(data), address))
+   print('内容: {}'.format(data.decode('utf-8')))
 
    # クライアントへのデータ送信
    if data:
       sent = sock.sendto(data, address)
-      print('sent {} bytes back to {}'.format(sent, address))
+      print('メッセージを送信 {} バイト:  {}'.format(sent, address))
